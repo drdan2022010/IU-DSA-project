53d309d8b7eefa8868a4fea83910a240cb3ca8d7
ITITIU20311 <ITITIU20311@student.hcmiu.edu.vn>
2023-05-21 02:24:52 +0700

    Update GameSquare.java

--- src/UserInterface/GameSquare.java
+++ src/UserInterface/GameSquare.java
@@ -2,6 +2,28 @@ package UserInterface;
 
 import javax.swing.*;
 import java.net.URL;
-public class GameSquare {
 
+
+public abstract class GameSquare extends JButton
+{
+
+    protected int xLocation;
+    protected int yLocation;
+    protected GameBoard board;
+
+    public GameSquare(int x, int y, URL filename, GameBoard board)
+    {
+        super(new ImageIcon(filename));
+
+        this.board = board;
+        xLocation = x;
+        yLocation = y;
+    }
+
+    public void setImage(URL filename)
+    {
+        this.setIcon(new ImageIcon(filename));
+    }
+
+    public abstract void clicked();
 }
